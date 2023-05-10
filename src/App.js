4ab713fa37d793b4cc93790411cae7874c5d32cd
Andressa Salgado <andressa.salgado@outlook.com>
2023-05-09 23:37:55 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MovieForm from './components/movieform';
+
+function App() {
+  return (
+    <MovieForm/>
+  );
+}
+
+export default App;
